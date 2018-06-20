019b4f9ee89068c6e4313f899ce0c76813596c3d
lordavadon2 <lordavadon22@gmail.com>
2018-06-20 11:07:31 +0300

    test1

---
+++ src/main/java/domain/Domain.java
@@ -0,0 +1,59 @@
+package domain;
+
+import bl.HibernateUtil;
+import entity.Address;
+import entity.Employee;
+import entity.Project;
+import service.AddressService;
+import service.EmployeeService;
+import service.ProjectService;
+
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.*;
+
+/**
+ * Created by vserdiuk on 2/8/2017.
+ */
+public class Domain {
+
+    public static void main(String[] args) throws SQLException {
+        AddressService addressService = new AddressService();
+        EmployeeService employeeService = new EmployeeService();
+        ProjectService projectService = new ProjectService();
+
+        Address address = new Address();
+        address.setCountry("DC");
+        address.setCity("Gotham city");
+        address.setStreet("Arkham street 1");
+        address.setPostCode("12345");
+
+        Project project = new Project();
+        project.setTitle("Gotham PD");
+
+        Employee employee = new Employee();
+        employee.setFirstName("James");
+        employee.setLastName("Gordon");
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1939, Calendar.MAY, 1);
+
+        employee.setBirthday(new Date(calendar.getTime().getTime()));
+        employee.setAddress(address);
+
+        Set<Employee> employees = new HashSet<Employee>();
+        employees.add(employee);
+        project.setEmployees(employees);
+
+        Set<Project> projects = new HashSet<Project>();
+        projects.add(project);
+        employee.setProjects(projects);
+
+        addressService.add(address);
+        employeeService.add(employee);
+        projectService.add(project);
+
+        HibernateUtil.shutdown();
+    }
+
+}
